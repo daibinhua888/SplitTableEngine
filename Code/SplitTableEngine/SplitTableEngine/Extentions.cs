168004ac1ff1f125b787550a028c76760c12bea3
McKay <daibinhua@gmail.com>
2015-06-03 13:47:34 +0800

    优化ToDictionary函数

--- Code/SplitTableEngine/SplitTableEngine/Extentions.cs
+++ Code/SplitTableEngine/SplitTableEngine/Extentions.cs
@@ -9,13 +9,27 @@ namespace SplitTableEngine
 {
     public static class Extentions
     {
+        private static Dictionary<Type, PropertyInfo[]> typeProperties = new Dictionary<Type, PropertyInfo[]>();
+
+        private static PropertyInfo[] GetGetProperties(Type type)
+        {
+            lock (typeProperties)
+            {
+                if(typeProperties.ContainsKey(type))
+                    return typeProperties[type];
+
+                PropertyInfo[] pi = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                typeProperties[type] = pi;
+
+                return pi;
+            }
+        }
+
         public static Dictionary<string, object> ToDictionary(this object obj)
         {
             Dictionary<String, object> map = new Dictionary<string, object>();
 
-            Type t = obj.GetType();
-
-            PropertyInfo[] pi = t.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var pi = GetGetProperties(obj.GetType());
 
             foreach (PropertyInfo p in pi)
             {
